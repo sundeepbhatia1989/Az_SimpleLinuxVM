250059d3cd2f7c09ce19f9ddf1830daccfcdd157
sundeepbhatia1989 <54140148+sundeepbhatia1989@users.noreply.github.com>
2023-01-19 10:39:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -172,14 +172,12 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     boot_diagnostics {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
-*/
-*/
+
     tags = {
         environment = "Terraform Demo"
     }
 }
-*/
-*/
+
 
 */
 /*Some important commands and links
