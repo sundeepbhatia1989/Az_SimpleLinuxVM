432fff5ee8d649db6bb3fbe55b8bc1c7371b94b2
sundeepbhatia1989 <54140148+sundeepbhatia1989@users.noreply.github.com>
2023-01-19 11:00:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,11 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "81d74218-6f1e-4599-9ff5-eee7b8bbf6e0"
-  client_id       = "67b94aeb-1f21-4507-b162-64421045708f"
-  client_secret   = "GA38Q~Wte13JIGDhJxa6RmfqLd._g_5zfiZF3bAH"
+  client_id       = "5b1ef92d-fb01-4acb-8e7b-6d609c821130"
+  client_secret   = "ds98Q~4jCIdOjbieEUlIDF4-Xbw1mrL3~-J7UcWq"
   tenant_id       = "af2262e1-ebf8-45fc-bb9e-0d0f9b0b4ec1"
 }
+
 
 # AUTH - Authenticating to Azure using a Service Principal and a Client Secret
 # Create a resource group if it doesn't exist
